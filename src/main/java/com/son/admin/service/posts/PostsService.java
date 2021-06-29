cd18f0741c1e7053e7118739f9d3ec961b54f388
wltnthss <60785586+wltnthss@users.noreply.github.com>
2021-06-29 23:46:12 +0900

    mustache 등록, 수정 기능추가

--- src/main/java/com/son/admin/service/posts/PostsService.java
+++ src/main/java/com/son/admin/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.son.admin.service.posts;
 
 import com.son.admin.domain.posts.Posts;
 import com.son.admin.domain.posts.PostsRepository;
+import com.son.admin.web.dto.PostsListResponseDto;
 import com.son.admin.web.dto.PostsResponseDto;
 import com.son.admin.web.dto.PostsSaveRequestDto;
 import com.son.admin.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -26,6 +30,13 @@ public class PostsService {
         posts.update(requestDto.getTitle(), requestDto.getContent());
 
         return id;
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 
     public PostsResponseDto findById (Long id){
